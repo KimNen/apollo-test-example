f01a2154c6f54231677fc6794b417f77ed5934a1
Theo <theo4284@demoday.co.kr>
2019-01-10 17:58:36 +0900

    리뷰 페이지 틀 제작

---
+++ src/pages/review/Review.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Review extends Component {
+    render() {
+        return (
+            <div>
+                리뷰 페이지
+            </div>
+        );
+    }
+}
+
+export default Review;
